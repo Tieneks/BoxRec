2497943e01fd2cc6de3fbd8af144703705ca4a8f
WMCoomans <wouter.coomans@futurefacts.nl>
2018-03-21 18:11:56 +0100

    extra features: height, reach, stance, age, won, lost, drawn, KOs

--- boxrec/models.py
+++ boxrec/models.py
@@ -11,7 +11,16 @@ class Fight(object):
         self, event_id, fight_id,
         boxer_left_id, boxer_right_id,
         hist_rating_left, hist_rating_right,
+        curr_rating_left, curr_rating_right,
+        stance_left='unknown',stance_right='unknown',
+        age_left='QQQ', age_right='QQQ',
         boxer_left=None, boxer_right=None,
+        height_left='QQQ',height_right='QQQ',
+        reach_left='QQQ',reach_right='QQQ',
+        won_left='QQQ', won_right='QQQ',
+        lost_left='QQQ', lost_right='QQQ',
+        drawn_left='QQQ', drawn_right='QQQ',
+        KO_left='QQQ', KO_right='QQQ',
         winner='left'
     ):
         self.event_id = event_id
@@ -22,6 +31,24 @@ class Fight(object):
         self.boxer_left = boxer_left
         self.hist_rating_left = hist_rating_left
         self.hist_rating_right = hist_rating_right
+        self.curr_rating_left = curr_rating_left
+        self.curr_rating_right = curr_rating_right
+        self.stance_left = stance_left
+        self.stance_right = stance_right
+        self.age_left = age_left
+        self.age_right = age_right
+        self.height_left = height_left
+        self.height_right = height_right
+        self.reach_left = reach_left
+        self.reach_right = reach_right
+        self.won_left = won_left
+        self.won_right = won_right
+        self.drawn_left = drawn_left
+        self.drawn_right = drawn_right
+        self.lost_left = lost_left
+        self.lost_right = lost_right
+        self.KO_left = KO_left
+        self.KO_right = KO_right
         self.winner = winner
 
     @property
@@ -52,5 +79,4 @@ class Fight(object):
             return self.boxer_left
         else:
             return self.boxer_right
-
 
--- boxrec/parsers.py
+++ boxrec/parsers.py
@@ -68,6 +68,17 @@ class FightParser(BaseParser):
 
         return rating_left, rating_right
 
+    def get_rating_after_fight(self, tree):
+        """ New function to determine rating after fight."""
+        rating_row = tree.xpath(
+             FightParser.BASE_DOM_PATH + \
+                '[./td/b/text() = "after fight"]/td[position() = 1 or position() =3]'  
+        )
+        rating_left = self.clean_rating(rating_row[0].text)
+        rating_right = self.clean_rating(rating_row[1].text)
+
+        return rating_left, rating_right
+
     def get_fight_outcome(self, tree, left_id, right_id):
         outcome = tree.xpath(
             FightParser.BASE_DOM_PATH + '//td[./span[@class="textWon"]]/a[./img]/@href'
@@ -89,12 +100,129 @@ class FightParser(BaseParser):
         else:
             return 'right'
 
+    def get_stance(self,tree):
+        stance_row = tree.xpath(
+            FightParser.BASE_DOM_PATH +
+                '[./td/b/text() = "stance"]/td[position() = 1 or position() =3]'
+        )
+        stance_left = stance_row[0].text
+        stance_right = stance_row[1].text
+
+        return stance_left, stance_right
+        
+    def get_age(self,tree):
+        age_row = tree.xpath(
+            FightParser.BASE_DOM_PATH +
+                '[./td/b/text() = "age"]/td[position() = 1 or position() =3]'
+        )
+        try:
+            age_left = age_row[0].text
+            age_right = age_row[1].text
+        except:
+            age_left = 'QQQ'
+            age_right = 'QQQ'
+
+        return age_left, age_right
+
+    def get_height(self,tree):
+        height_row = tree.xpath(
+            FightParser.BASE_DOM_PATH +
+                '[./td/b/text() = "height"]/td[position() = 1 or position() =3]'
+        )
+        try:
+            height_left = height_row[0].text.rsplit('/')[1].strip().replace('cm','')
+            height_right = height_row[1].text.rsplit('/')[1].strip().replace('cm','')
+        except:
+            height_left = 'QQQ'
+            height_right = 'QQQ'
+        
+        return height_left, height_right
+
+    def get_reach(self,tree):
+        reach_row = tree.xpath(
+            FightParser.BASE_DOM_PATH +
+                '[./td/b/text() = "reach"]/td[position() = 1 or position() =3]'
+        )
+        try:
+            reach_left = reach_row[0].text.rsplit('/')[1].strip().replace('cm','')
+            reach_right = reach_row[1].text.rsplit('/')[1].strip().replace('cm','')
+        except:
+            reach_left = 'QQQ'
+            reach_right = 'QQQ'
+
+        return reach_left, reach_right
+
+    def get_won(self,tree):
+        won_row = tree.xpath(
+            FightParser.BASE_DOM_PATH +
+                '[./td/b/text() = "won"]/td[position() = 1 or position() =3]'
+        )
+        try:
+            won_left = won_row[0].text
+            won_right = won_row[1].text
+        except:
+            won_left = 'QQQ'
+            won_right = 'QQQ'
+
+        return won_left, won_right
+
+    def get_lost(self,tree):
+        lost_row = tree.xpath(
+            FightParser.BASE_DOM_PATH +
+                '[./td/b/text() = "lost"]/td[position() = 1 or position() =3]'
+        )
+        try:
+            lost_left = lost_row[0].text
+            lost_right = lost_row[1].text
+        except:
+            lost_left = 'QQQ'
+            lost_right = 'QQQ'
+
+        return lost_left, lost_right
+        
+    def get_drawn(self,tree):
+        drawn_row = tree.xpath(
+            FightParser.BASE_DOM_PATH +
+                '[./td/b/text() = "drawn"]/td[position() = 1 or position() =3]'
+        )
+        try:
+            drawn_left = drawn_row[0].text
+            drawn_right = drawn_row[1].text
+        except:
+            drawn_left = 'QQQ'
+            drawn_right = 'QQQ'
+
+        return drawn_left, drawn_right
+        
+    def get_KO(self,tree):
+        KO_row = tree.xpath(
+            FightParser.BASE_DOM_PATH +
+                '[./td/b/text() = "KOs"]/td[position() = 1 or position() =3]'
+        )
+        try:
+            KO_left = KO_row[0].text
+            KO_right = KO_row[1].text
+        except:
+            KO_left = 'QQQ'
+            KO_right = 'QQQ'
+
+        return KO_left, KO_right
+        
     def parse(self, response):
         tree = self.make_dom_tree(response)
 
         event_id, fight_id = self.get_event_and_fight_id(response.url)
         boxer_left_id, boxer_right_id = self.get_boxer_ids(tree)
-        rating_left, rating_right = self.get_rating_before_fight(tree)
+        rating_before_left, rating_before_right = self.get_rating_before_fight(tree)
+        rating_after_left, rating_after_right = self.get_rating_after_fight(tree)
+        stance_left, stance_right = self.get_stance(tree)
+        age_left, age_right = self.get_age(tree)
+        height_left, height_right = self.get_height(tree)
+        reach_left, reach_right = self.get_reach(tree)
+        won_left, won_right = self.get_won(tree)
+        lost_left, lost_right = self.get_lost(tree)
+        drawn_left, drawn_right = self.get_drawn(tree)
+        KO_left, KO_right = self.get_KO(tree)
         result = self.get_fight_outcome(tree, boxer_left_id, boxer_right_id)
 
         return Fight(
@@ -102,8 +230,26 @@ class FightParser(BaseParser):
             fight_id=fight_id,
             boxer_left_id=boxer_left_id,
             boxer_right_id=boxer_right_id,
-            hist_rating_left=rating_left,
-            hist_rating_right=rating_right,
+            hist_rating_left=rating_before_left,
+            hist_rating_right=rating_before_right,
+            curr_rating_left=rating_after_left,
+            curr_rating_right=rating_after_right,
+            stance_left=stance_left,
+            stance_right=stance_right,
+            age_left=age_left,
+            age_right=age_right,
+            height_left=height_left,
+            height_right=height_right,
+            reach_left=reach_left,
+            reach_right=reach_right,
+            won_left=won_left,
+            won_right=won_right,
+            drawn_left=drawn_left,
+            drawn_right=drawn_right,
+            lost_left=lost_left,
+            lost_right=lost_right,
+            KO_left=KO_left,
+            KO_right=KO_right,
             winner=result
         )
 
